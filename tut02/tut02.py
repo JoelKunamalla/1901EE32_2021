d0811c8b974f7e78c1b604eba9cedbbb66eedf94
JoelKunamalla <kunamallajoel0@gmail.com>
2021-08-21 12:44:32 +0530

    solution for tut2

---
+++ tut02/tut02.py
@@ -0,0 +1,32 @@
+def comp(input) :
+    i=0
+    yu = False
+    iy =[]
+    while( i < len(input)):
+        y= input[i]
+        if(type(y)!= int):
+            yu = True
+            iy.append(input[i])
+        i=i+1
+    if(yu == True):
+        print("Please enter a valid input list.  Invalid inputs detected :" , iy)
+          
+    else :
+        az = []
+        c=0
+        i=0
+        while(i < len(input)) :
+            cv= False
+            for x in az :
+                if(x == input[i] ):
+                    c= c+1
+                    cv=True
+            if(cv==False):        
+             az.append(input[i])        
+            if(len(az) > 5):
+             az.pop(0)
+           
+            i= i+1
+        print( "score:" , c)
+input = [1,4, "a", 3, 5, "Star", 7.5]
+comp(input)
